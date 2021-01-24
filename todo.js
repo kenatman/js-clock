94420370cae28d06ec30bccb6b18f69f138deec9
kenatman <76776206+kenatman@users.noreply.github.com>
2021-01-24 13:49:35 +0900

    toDoList pt2 too hard

--- todo.js
+++ todo.js
@@ -4,15 +4,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("btn");
-  delBtn.innerText = "𝓧";
+
   const span = document.createElement("span");
+  const newID = toDos.length + 1;
+  delBtn.innerText = "𝓧";
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newID;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newID,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handlesubmit(event) {
@@ -23,8 +38,12 @@ function handlesubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
